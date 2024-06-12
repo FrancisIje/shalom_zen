8de4a40ed4a4bd24364119aad2a4afde44810f28
FrancisIje <ijenebefrancis@gmail.com>
2024-06-12 18:34:54 +0100

    onboarding screen

--- lib/onboarding/onboarding.dart
+++ lib/onboarding/onboarding.dart
@@ -1,10 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        children: [
+          SizedBox(
+            height: double.maxFinite,
+            width: double.maxFinite,
+            child: Image.asset(
+              "assets/shalom_background3.png",
+              fit: BoxFit.fill,
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Expanded(child: Gap(10)),
+              SizedBox(
+                  height: 100,
+                  width: 96,
+                  child: Image.asset("assets/shalom_zen_logo.png")),
+              const Gap(220),
+              const Align(
+                alignment: Alignment.center,
+                child: Text(
+                  "Your Personalized \nMeditation Oasis",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white),
+                ),
+              ),
+              const Gap(32),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pushNamedAndRemoveUntil(
+                      "/signin",
+                      (route) => true,
+                    );
+                  },
+                  child: Text(
+                    "Get Started",
+                    style: const TextStyle().copyWith(
+                      color: const Color(0xFF0D1915),
+                    ),
+                  )),
+              const Gap(140),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
