a4eb312176d3eb4bc178025ba845517176228ada
FrancisIje <ijenebefrancis@gmail.com>
2024-06-24 22:37:16 +0100

    ui changes

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:shalom_zen/enums/tile_audio_source.dart';
+import 'package:shalom_zen/player/sound_player.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -42,92 +44,110 @@ class HomePage extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Container(
-                width: MediaQuery.of(context).size.width * 0.44,
-                height: 200,
-                padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-                decoration: const BoxDecoration(
-                    color: Color(0xFFD7F7A2),
-                    borderRadius: BorderRadius.all(Radius.circular(15))),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      "Pure Hz to Relax",
-                      style: TextStyle(
-                          fontFamily: "Aeonik",
-                          fontWeight: FontWeight.w500,
-                          fontSize: 24,
-                          color: Color(0xFF0D1915)),
-                    ),
-                    const Gap(42),
-                    Row(
-                      children: [
-                        const Text(
-                          "15 min",
-                          style: TextStyle(
-                              fontFamily: "Aeonik",
-                              fontSize: 14,
-                              fontWeight: FontWeight.w400,
-                              color: Colors.black),
-                        ),
-                        const Gap(16),
-                        Container(
-                            height: 70,
-                            width: 62,
-                            decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: const Color(0xFF0D191526)
-                                    .withOpacity(0.15)),
-                            child: const Icon(
-                              Icons.play_arrow_rounded,
-                              color: Colors.white,
-                            ))
-                      ],
-                    )
-                  ],
+              GestureDetector(
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => const SoundPlayer(
+                        title: "528 Hz",
+                        subtitle: "Relax Frequency",
+                        audioUrl: "assets/Manifest_Your_Greatness_528hz.mp3",
+                        audioSource: TileAudioSource.assets),
+                  ));
+                },
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.44,
+                  height: 200,
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                  decoration: const BoxDecoration(
+                      color: Color(0xFFD7F7A2),
+                      borderRadius: BorderRadius.all(Radius.circular(15))),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "Pure Hz to Relax",
+                        style: TextStyle(
+                            fontFamily: "Aeonik",
+                            fontWeight: FontWeight.w500,
+                            fontSize: 24,
+                            color: Color(0xFF0D1915)),
+                      ),
+                      Expanded(child: const Gap(2)),
+                      Row(
+                        children: [
+                          const Text(
+                            "Listen now",
+                            style: TextStyle(
+                                fontFamily: "Aeonik",
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.black),
+                          ),
+                          Expanded(child: const Gap(16)),
+                          Container(
+                              height: 70,
+                              width: MediaQuery.of(context).size.width * 0.1,
+                              decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: const Color(0xFF0D191526)
+                                      .withOpacity(0.15)),
+                              child: const Icon(
+                                Icons.play_arrow_rounded,
+                                color: Colors.white,
+                              ))
+                        ],
+                      )
+                    ],
+                  ),
                 ),
               ),
-              Container(
-                width: MediaQuery.of(context).size.width * 0.44,
-                height: 200,
-                padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-                decoration: BoxDecoration(
-                    color: Colors.white.withOpacity(0.15),
-                    borderRadius: const BorderRadius.all(Radius.circular(15))),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      "Focus\nMantra",
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                          fontFamily: "Aeonik",
-                          fontWeight: FontWeight.w500,
-                          fontSize: 24,
-                          color: Colors.white),
-                    ),
-                    const Gap(42),
-                    Row(
-                      children: [
-                        const Text(
-                          "10 min",
-                          style: TextStyle(
-                              fontFamily: "Aeonik",
-                              fontSize: 14,
-                              fontWeight: FontWeight.w400,
-                              color: Colors.white),
-                        ),
-                        const Gap(16),
-                        Container(
-                            height: 70,
-                            width: 62,
-                            decoration: const BoxDecoration(
-                                shape: BoxShape.circle, color: Colors.white),
-                            child: const Icon(Icons.play_arrow_rounded))
-                      ],
-                    )
-                  ],
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.44,
+                  height: 200,
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                  decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.15),
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(15))),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "Record \nMantra",
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                            fontFamily: "Aeonik",
+                            fontWeight: FontWeight.w500,
+                            fontSize: 24,
+                            color: Colors.white),
+                      ),
+                      Expanded(child: const Gap(2)),
+                      Row(
+                        children: [
+                          const Text(
+                            "Start recording",
+                            style: TextStyle(
+                                fontFamily: "Aeonik",
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.white),
+                          ),
+                          const Expanded(child: Gap(4)),
+                          const Gap(16),
+                          Container(
+                              height: 70,
+                              width: MediaQuery.of(context).size.width * 0.1,
+                              decoration: const BoxDecoration(
+                                  shape: BoxShape.circle, color: Colors.white),
+                              child: const Icon(Icons.mic))
+                        ],
+                      )
+                    ],
+                  ),
                 ),
               ),
             ],
