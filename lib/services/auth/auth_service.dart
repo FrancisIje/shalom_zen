949a024e6fcc30349347da5ebe82a7ea44f83ade
FrancisIje <ijenebefrancis@gmail.com>
2024-07-03 00:33:51 +0100

    feature: authentication services

---
+++ lib/services/auth/auth_service.dart
@@ -0,0 +1,35 @@
+import 'package:shalom_zen/services/auth/auth_provider.dart';
+import 'package:shalom_zen/services/auth/auth_user.dart';
+import 'package:shalom_zen/services/auth/firebase_auth.dart';
+
+class AuthService implements AuthProvider {
+  final AuthProvider authProvider;
+  const AuthService(this.authProvider);
+
+  factory AuthService.firebase() => AuthService(FirebaseAuthProvider());
+  @override
+  Future<AuthUser> createUser(
+          {required String email, required String password}) =>
+      authProvider.createUser(email: email, password: password);
+
+  @override
+  // TODO: implement currentUser
+  AuthUser? get currentUser => throw UnimplementedError();
+
+  @override
+  Future<void> initialize() => authProvider.initialize();
+
+  @override
+  Future<AuthUser> logIn({required String email, required String password}) =>
+      authProvider.logIn(email: email, password: password);
+
+  @override
+  Future<void> logOut() => authProvider.logOut();
+
+  @override
+  Future<void> sendEmailVerification() => authProvider.sendEmailVerification();
+
+  @override
+  Future<void> sendPasswordReset({required String toEmail}) =>
+      authProvider.sendPasswordReset(toEmail: toEmail);
+}
