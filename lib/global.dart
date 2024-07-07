f0b5d636876748f4d6605d047035967269878c2d
FrancisIje <ijenebefrancis@gmail.com>
2024-07-08 00:56:24 +0100

    global var

---
+++ lib/global.dart
@@ -0,0 +1,12 @@
+import 'package:record/record.dart';
+
+AudioRecorder appAudioRecorder = AudioRecorder();
+
+List<Map<String, String>> appFreqSounds = [
+  {
+    "417Hz": "assets/417Hz.mp3",
+  },
+  {"432Hz": "assets/432Hz.mp3"},
+  {"639Hz": "assets/639Hz.mp3"},
+  {"528Hz": "assets/Manifest_Your_Greatness_528hz.mp3"}
+];
