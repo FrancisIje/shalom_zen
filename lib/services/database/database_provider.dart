c1924b6d4a2948c8d74cbaa3a659b4001c44909d
FrancisIje <ijenebefrancis@gmail.com>
2024-07-11 11:23:45 +0100

    fix: added update, delete, get user and add,remove, stream rec

--- lib/services/database/database_provider.dart
+++ lib/services/database/database_provider.dart
@@ -1,4 +1,5 @@
 import 'package:shalom_zen/models/user_model.dart';
+import 'package:shalom_zen/models/user_recordings.dart';
 
 abstract class DatabaseProvider {
   Future<void> createUser(
@@ -6,4 +7,10 @@ abstract class DatabaseProvider {
       required String firstName,
       required String lastName,
       required String password});
+  Future<ZenUser> getUser();
+  Future<void> updateUser();
+  Future<void> deleteUser();
+  Future<void> addRecording(UserRecordings rec);
+  Future<void> removeRecording(String recUrl);
+  Stream<List<UserRecordings>> getRecordingsStream();
 }
