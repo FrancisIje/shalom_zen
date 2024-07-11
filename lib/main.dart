2349264e7385c21421d2b6b8cb8451bc002b379e
FrancisIje <ijenebefrancis@gmail.com>
2024-07-11 11:33:31 +0100

    firebase setup and named routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,51 @@
 import "package:flutter/material.dart";
+import "package:shalom_zen/auth/signin.dart";
+import "package:shalom_zen/auth/signup.dart";
+import "package:shalom_zen/home/home_screen.dart";
 import "package:shalom_zen/onboarding/onboarding.dart";
 import "package:shalom_zen/splash/splash_screen.dart";
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(ShalomZen());
+// ...
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const ShalomZen());
 }
 
-class ShalomZen extends StatelessWidget {
+class ShalomZen extends StatefulWidget {
   const ShalomZen({super.key});
 
   @override
+  State<ShalomZen> createState() => _ShalomZenState();
+}
+
+class _ShalomZenState extends State<ShalomZen> {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       routes: {
         "/onboarding": (context) => const OnboardingScreen(),
+        "/signup": (context) => const SignUpScreen(),
+        "/signin": (context) => const SignInScreen(),
+        "/home_screen": (context) => const HomeScreen(),
       },
       theme: ThemeData(
           fontFamily: "Aeonik",
           elevatedButtonTheme: const ElevatedButtonThemeData(
               style: ButtonStyle(
+                  fixedSize: WidgetStatePropertyAll(Size(double.maxFinite, 61)),
                   shape: WidgetStatePropertyAll(RoundedRectangleBorder(
                       borderRadius: BorderRadius.all(Radius.circular(13)))),
                   textStyle: WidgetStatePropertyAll(
-                      TextStyle(color: Color(0xFF0D1915))),
+                      TextStyle(fontSize: 24, fontWeight: FontWeight.w400)),
                   backgroundColor: WidgetStatePropertyAll(Color(0xFFD7F7A2))))),
-      home: SplashScreen(),
+      home: const SplashScreen(),
     );
   }
 }
