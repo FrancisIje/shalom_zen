b9bce8ccdd3526e4e64de07b4bc56b110f8e9486
FrancisIje <ijenebefrancis@gmail.com>
2024-07-11 11:22:03 +0100

    refactor

--- lib/services/database/firebase_cloud_database.dart
+++ lib/services/database/firebase_cloud_database.dart
@@ -1,12 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:shalom_zen/models/user_model.dart';
+import 'package:shalom_zen/models/user_recordings.dart';
 import 'package:shalom_zen/services/database/database_provider.dart';
 
 class FirestoreProvider implements DatabaseProvider {
   final CollectionReference _users =
       FirebaseFirestore.instance.collection('users');
-  final uid = FirebaseAuth.instance.currentUser!.uid;
+  final uid = FirebaseAuth.instance.currentUser?.uid ?? "";
 
   @override
   Future<void> createUser({
@@ -16,12 +17,13 @@ class FirestoreProvider implements DatabaseProvider {
     required String password,
   }) async {
     await _users.doc(uid).set(ZenUser(
-        id: uid,
-        firstName: firstName,
-        lastName: lastName,
-        email: email,
-        profileUrl: null,
-        password: password));
+          id: uid,
+          firstName: firstName,
+          lastName: lastName,
+          email: email,
+          profileUrl: null,
+          password: password,
+        ).toFirestore());
   }
 
   @override
@@ -38,18 +40,64 @@ class FirestoreProvider implements DatabaseProvider {
   @override
   Future<void> updateUser(
       {String? firstName, String? lastName, String? profileUrl}) async {
-    final DocumentReference userRef = _users.doc(uid);
+    try {
+      final DocumentReference userRef = _users.doc(uid);
 
-    final Map<String, dynamic> updateData = {};
-    if (firstName != null) updateData['firstName'] = firstName;
-    if (lastName != null) updateData['lastName'] = lastName;
-    if (profileUrl != null) updateData['profileUrl'] = profileUrl;
+      final Map<String, dynamic> updateData = {};
+      if (firstName != null && firstName.isNotEmpty) {
+        updateData[ZenUserConst.firstName] = firstName;
+      }
+      if (lastName != null && lastName.isNotEmpty) {
+        updateData[ZenUserConst.lastName] = lastName;
+      }
+      if (profileUrl != null && profileUrl.isNotEmpty) {
+        updateData[ZenUserConst.profileUrl] = profileUrl;
+      }
 
-    await userRef.update(updateData);
+      await userRef.update(updateData);
+    } on FirebaseException {
+      rethrow;
+    }
   }
 
   @override
   Future<void> deleteUser() async {
     await _users.doc(uid).delete();
+  }
+
+  @override
+  Future<void> addRecording(UserRecordings rec) async {
+    // TODO: implement addRecording
+    await _users
+        .doc(uid)
+        .collection(ZenUserConst.userRecordings)
+        .add(rec.toMap());
+  }
+
+  @override
+  Future<void> removeRecording(String recUrl) async {
+    // TODO: implement removeRecording
+    final recordDetails = await _users
+        .doc(uid)
+        .collection(ZenUserConst.userRecordings)
+        .where(RecordingConst.downloadUrl, isEqualTo: recUrl)
+        .get();
+    final recId = recordDetails.docs.first.id;
+    await _users
+        .doc(uid)
+        .collection(ZenUserConst.userRecordings)
+        .doc(recId)
+        .delete();
+  }
+
+  @override
+  Stream<List<UserRecordings>> getRecordingsStream() {
+    return _users
+        .doc(uid)
+        .collection(ZenUserConst.userRecordings)
+        .snapshots()
+        .map((snapshot) => snapshot.docs
+            .map((doc) => UserRecordings.fromMap(doc.data()))
+            .toList());
   }
 }
